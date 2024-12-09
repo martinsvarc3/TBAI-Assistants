e141ee0228fb84764969db6862e851db94e5700b
jantrej <jantrejbal7@gmail.com>
2024-12-10 00:14:08 +0100

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'cdn.prod.website-files.com',
+      'res.cloudinary.com'
+    ],
+  },
+};
+
+export default nextConfig;
