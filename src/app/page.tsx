fc78985b77059d2c3c0366902b453c7a3a3b345f
jantrej <jantrejbal7@gmail.com>
2024-12-10 00:45:11 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+'use client'
+
 import CharacterSelection from "@/components/ui/CharacterSelection";
 
 export default function Home() {
   return (
-    
-      
-    
+    <main className="min-h-screen bg-gray-100">
+      <CharacterSelection />
+    </main>
   );
 }
