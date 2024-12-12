4bc6ddfc66c944943247149c2cda262803e80aba
jantrej <jantrejbal7@gmail.com>
2024-12-12 16:03:48 +0100

    Update CharacterSelection.tsx

--- src/components/ui/CharacterSelection.tsx
+++ src/components/ui/CharacterSelection.tsx
@@ -366,6 +366,7 @@ return (
                   ? '#FAA248'
                   : '#EC1B26'
                  }`
+                boxShadow: '0 4px 6px rgba(0, 0, 0, 0.08), 0 1px 3px rgba(0, 0, 0, 0.1)'
               }}
             >
             <div className="p-4 flex flex-col items-center text-center">
